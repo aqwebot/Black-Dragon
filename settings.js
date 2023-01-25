daed0ecde4f057659932669366339c47b87cf9ab
aqwebot <122733543+aqwebot@users.noreply.github.com>
2023-01-25 10:46:49 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,16 +14,16 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94786802371'] //ur owner number
-global.ownername = "Nimesh Official" //ur owner name
-global.ytname = "YT: HELA CLOUD" //ur yt chanel name
+global.owner = ['94716549944'] //ur owner number
+global.ownername = "Pasan Lakshan" //ur owner name
+global.ytname = "YT: pasan tech" //ur yt chanel name
 global.socialm = "GitHub: nimaofficial" //ur github or insta name
 global.location = "Sri Lanka, Colombo, Kiribathgoda" //ur location
 
 //bot bomdy 
-global.ownernomer = "94786802371" //ur number
-global.premium = ['94786802371'] //ur premium number
-global.botname = 'BLACK DRAGON' //ur bot name
+global.ownernomer = "94716549944" //ur number
+global.premium = ['94716549944'] //ur premium number
+global.botname = 'PASAN BOT' //ur bot name
 global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
 global.websitex = "https://www.helacloud.ga" //ur website to be displayed
 global.botscript = 'https://github.com/nimaofficial/Black-Dragon' //script link
